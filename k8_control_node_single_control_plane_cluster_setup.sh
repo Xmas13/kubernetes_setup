c94d246d97e2ea8ab7ede1efac85e3e1369e3b6f
Noel Miller <noelmiller@protonmail.com>
2020-07-02 15:50:48 -0500

    Fixed issues pod-network-cidr not being defined

--- k8_control_node_single_control_plane_cluster_setup.sh
+++ k8_control_node_single_control_plane_cluster_setup.sh
@@ -3,17 +3,17 @@
 # Enter password as arg 1, otherwise will be assigned blank password
 K8USERPASSWORD=$1
 
-# Starts setup of Control Node
-kubeadm init
+# Starts setup of Control Node. Creates Pod Network to use
+kubeadm init --pod-network-cidr=192.168.0.0/16
 
 # Create User to administrate with kubectl
 useradd k8user
 usermod -aG wheel k8user
 echo $K8USERPASSWORD | passwd k8user --stdin
 K8HOME=/home/k8user
-mkdir -p $K8HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $K8HOME/.kube/config
-sudo chown $(k8user):$(k8user) $K8HOME/.kube/config
+mkdir -p ${K8HOME}/.kube
+sudo cp -i /etc/kubernetes/admin.conf ${K8HOME}/.kube/config
+sudo chown k8user:k8user ${K8HOME}/.kube/config
 
 # Install Calico
 su - k8user -c "kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml"
